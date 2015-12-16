75ba0faca91b0646557eedc6c91cc74acd0c98e2
Izloko89 <viciz.1089@gmail.com>
2015-12-16 15:36:42 -0600

    pdf terminado

--- scripts/pdf_cotizacion.php
+++ scripts/pdf_cotizacion.php
@@ -7,33 +7,33 @@ include_once("func_form.php");
 $emp=$_SESSION["id_empresa"];
 $id = 0;
 if(isset($_GET["id"])){
-	$id=$_GET["id"];
+    $id=$_GET["id"];
 }
 
 //funciones para convertir px->mm
 function mmtopx($d){
-	$fc=96/25.4;
-	$n=$d*$fc;
-	return $n."px";
+    $fc=96/25.4;
+    $n=$d*$fc;
+    return $n."px";
 }
 function pxtomm($d){
-	$fc=96/25.4;
-	$n=$d/$fc;
-	return $n."mm";
+    $fc=96/25.4;
+    $n=$d/$fc;
+    return $n."mm";
 }
 function checkmark(){
-	$url="http://".$_SERVER["HTTP_HOST"]."/img/checkmark.png";
-	$s='<img src="'.$url.'" style="height:10px;" />';
-	return $s;
+    $url="http://".$_SERVER["HTTP_HOST"]."/img/checkmark.png";
+    $s='<img src="'.$url.'" style="height:10px;" />';
+    return $s;
 }
 function folio($digitos,$folio){
-	$usado=strlen($folio);
-	$salida="";
-	for($i=0;$i<($digitos-$usado);$i++){
-		$salida.="0";
-	}
-	$salida.=$folio;
-	return $salida;
+    $usado=strlen($folio);
+    $salida="";
+    for($i=0;$i<($digitos-$usado);$i++){
+        $salida.="0";
+    }
+    $salida.=$folio;
+    return $salida;
 }
 //tamaño carta alto:279.4 ancho:215.9
 $heightCarta=960;
@@ -45,103 +45,103 @@ $mmCartaW=pxtomm($widthCarta);
 ob_start();
 
 try{
-	$bd=new PDO($dsnw,$userw,$passw,$optPDO);
-	// para saber los datos del cliente
-	$sql="SELECT 
-		t1.id_cotizacion,
-		t1.nombre As nombreEvento,
-		t1.fecha,
-		t1.fechaevento,
-		t1.fechamontaje,
-		t1.fechadesmont,
-		t1.id_cliente,
-		t2.nombre,
-		t3.direccion,
-		t3.colonia,
-		t3.ciudad,
-		t3.estado,
-		t3.cp,
-		t3.telefono,
-		t1.noinvitados,
-		t1.dirEvento
-	FROM cotizaciones t1
-	LEFT JOIN clientes t2 ON t1.id_cliente=t2.id_cliente
-	LEFT JOIN clientes_contacto t3 ON t1.id_cliente=t3.id_cliente
-	WHERE id_cotizacion=$id;";
-	$res=$bd->query($sql);
-	$res1=$res->fetchAll(PDO::FETCH_ASSOC);
-	$noIn = $res1[0]["noinvitados"];
-	$evento=$res1[0];
-	$cliente=$evento["nombre"];
-	$telCliente=$evento["telefono"];
-	$nombreEve=$evento["nombreEvento"];
-	$domicilio=$evento["direccion"]." ".$evento["colonia"]." ".$evento["ciudad"]." ".$evento["estado"]." ".$evento["cp"];
-	$fecha=$evento["fecha"];
-	$fechaEve=$evento["fechaevento"];
-	$dirEve = $evento["dirEvento"];
-	//print_r($fecha);
-	
+    $bd=new PDO($dsnw,$userw,$passw,$optPDO);
+    // para saber los datos del cliente
+    $sql="SELECT 
+        t1.id_cotizacion,
+        t1.nombre As nombreEvento,
+        t1.fecha,
+        t1.fechaevento,
+        t1.fechamontaje,
+        t1.fechadesmont,
+        t1.id_cliente,
+        t2.nombre,
+        t3.direccion,
+        t3.colonia,
+        t3.ciudad,
+        t3.estado,
+        t3.cp,
+        t3.telefono,
+        t1.noinvitados,
+        t1.dirEvento
+    FROM cotizaciones t1
+    LEFT JOIN clientes t2 ON t1.id_cliente=t2.id_cliente
+    LEFT JOIN clientes_contacto t3 ON t1.id_cliente=t3.id_cliente
+    WHERE id_cotizacion=$id;";
+    $res=$bd->query($sql);
+    $res1=$res->fetchAll(PDO::FETCH_ASSOC);
+    $noIn = $res1[0]["noinvitados"];
+    $evento=$res1[0];
+    $cliente=$evento["nombre"];
+    $telCliente=$evento["telefono"];
+    $nombreEve=$evento["nombreEvento"];
+    $domicilio=$evento["direccion"]." ".$evento["colonia"]." ".$evento["ciudad"]." ".$evento["estado"]." ".$evento["cp"];
+    $fecha=$evento["fecha"];
+    $fechaEve=$evento["fechaevento"];
+    $dirEve = $evento["dirEvento"];
+    //print_r($fecha);
+    
 }catch(PDOException $err){
-	echo $err->getMessage();
+    echo $err->getMessage();
 }
 $bd=NULL;
 
 //para saber los articulos y paquetes
 try{
-	$bd=new PDO($dsnw,$userw,$passw,$optPDO);
-	$sql="SELECT
-		t1.*,
-		t2.nombre,
+    $bd=new PDO($dsnw,$userw,$passw,$optPDO);
+    $sql="SELECT
+        t1.*,
+        t2.nombre,
         t2.image
-	FROM cotizaciones_articulos t1
-	LEFT JOIN articulos t2 ON t1.id_articulo=t2.id_articulo
-	WHERE t1.id_cotizacion=$id;";
-	$res=$bd->query($sql);
-	$articulos=array();
-	foreach($res->fetchAll(PDO::FETCH_ASSOC) as $d){
-		if($d["id_articulo"]!=""){
-			$art=$d["id_item"];
-			unset($d["id_item"]);
-			$articulos[$art]=$d;
-		}else{
-			$art=$d["id_item"];
-			unset($d["id_item"]);
-			$articulos[$art]=$d;
-			$paq=$d["id_paquete"];
-			
-			//nombre del paquete
-			$sql="SELECT nombre FROM paquetes WHERE id_paquete=$paq;";
-			$res3=$bd->query($sql);
-			$res3=$res3->fetchAll(PDO::FETCH_ASSOC);
-			$articulos[$art]["nombre"]="PAQ. ".$res3[0]["nombre"];
-			
-			$sql="SELECT 
-				t1.cantidad,
-				t2.nombre
-			FROM paquetes_articulos t1
-			INNER JOIN articulos t2 ON t1.id_articulo=t2.id_articulo
-			WHERE id_paquete=$paq AND t2.perece=0;";
-			$res2=$bd->query($sql);
-			
-			foreach($res2->fetchAll(PDO::FETCH_ASSOC) as $dd){
-				$dd["precio"]="";
-				$dd["total"]="";
-				$dd["nombre"]=$dd["cantidad"]." ".$dd["nombre"];
-				$dd["cantidad"]="";
-				$articulos[]=$dd;
-			}
-		}
-	}
+    FROM cotizaciones_articulos t1
+    LEFT JOIN articulos t2 ON t1.id_articulo=t2.id_articulo
+    WHERE t1.id_cotizacion=$id;";
+    $res=$bd->query($sql);
+    $articulos=array();
+    foreach($res->fetchAll(PDO::FETCH_ASSOC) as $d){
+        if($d["id_articulo"]!=""){
+            $art=$d["id_item"];
+            unset($d["id_item"]);
+            $articulos[$art]=$d;
+        }else{
+            $art=$d["id_item"];
+            unset($d["id_item"]);
+            $articulos[$art]=$d;
+            $paq=$d["id_paquete"];
+            
+            //nombre del paquete
+            $sql="SELECT nombre FROM paquetes WHERE id_paquete=$paq;";
+            $res3=$bd->query($sql);
+            $res3=$res3->fetchAll(PDO::FETCH_ASSOC);
+            $articulos[$art]["nombre"]="PAQ. ".$res3[0]["nombre"];
+            
+            $sql="SELECT 
+                t1.cantidad,
+                t2.nombre
+            FROM paquetes_articulos t1
+            INNER JOIN articulos t2 ON t1.id_articulo=t2.id_articulo
+            WHERE id_paquete=$paq AND t2.perece=0;";
+            $res2=$bd->query($sql);
+            
+            foreach($res2->fetchAll(PDO::FETCH_ASSOC) as $dd){
+                $dd["precio"]="";
+                $dd["total"]="";
+                $dd["nombre"]=$dd["cantidad"]." ".$dd["nombre"];
+                $dd["cantidad"]="";
+                $articulos[]=$dd;
+            }
+        }
+    }
 }catch(PDOException $err){
-	echo $err->getMessage();
+    echo $err->getMessage();
 }
 
 //var_dump($articulos);
 
 $html='
 <page backbottom="15px">
-	
-	<page_footer>
+    
+    <page_footer>
         <table border="0" cellpadding="0" cellspacing="0" style="font-size:13px; width:100%; margin-top:30px; padding:0 20px;">
             <tr>
                 <td style="width:100%;vertical-align:top; text-align:center; border-top:'.pxtomm(2).' solid #484848;">
@@ -153,18 +153,18 @@ $html='
             </tr>
         </table>
     </page_footer>
-	
+    
 <style>
 span{
-	display:inline-block;
-	padding:10px;
+    display:inline-block;
+    padding:10px;
 }
 h1{
-	font-size:20px;
+    font-size:20px;
 }
 .spacer{
-	display:inline-block;
-	height:1px;
+    display:inline-block;
+    height:1px;
 }
 .div{
     color: #000;
@@ -211,7 +211,7 @@ font-family: "Pacifico", sans-serif;
         <tr>
             <td style="width:100%;">
                 <div style="width:100%; padding 20px; font-size:12px;text-align:justify;">
-                Espero que te encuentres muy bien. Antes que nada quiero agradecerte la oportunidad que nos brindas para participar contigo en la planeación de "'.$nombreEve.'". Sabemos que es un evento muy importante, por ello siéntete en plena confianza de preguntarme cualquier duda respecto al recorrido	culinario	y	servicios	adicionales	que	te	ofrecemos.</div>
+                Espero que te encuentres muy bien. Antes que nada quiero agradecerte la oportunidad que nos brindas para participar contigo en la planeación de "'.$nombreEve.'". Sabemos que es un evento muy importante, por ello siéntete en plena confianza de preguntarme cualquier duda respecto al recorrido culinario   y   servicios   adicionales que te  ofrecemos.</div>
             </td>
         </tr>
     </table>
@@ -219,16 +219,27 @@ font-family: "Pacifico", sans-serif;
         <tr>
             <td style="width:100%;">
                 <div style="width:100%; padding 20px; font-size:12px;text-align:justify;">
-                Hemos creado una historia gastronómica que iremos contando a los invitados a través de diversas creaciones	culinarias	que	degustarán	desde	su	llegada.	La	historia	dice	así...</div>
+                Hemos creado una historia gastronómica que iremos contando a los invitados a través de diversas creaciones  culinarias  que degustarán  desde   su  llegada.    La  historia    dice    así...</div>
             </td>
         </tr>
     </table>
     <br/>
     <div style="width:100%; padding:0 20px; text-align:justify;"><strong>Bienvenida de Invitados:</strong></div>
     <div style="width:100%; padding:0 20px; text-align:justify;">
-    <textarea cols="68" rows="4" style="width:100%; padding:0 20px; text-align:justify;border:none;">A la llegada de los invitados se ofrecerá una barra de aguas frescas acompañadas de una selección especial de canapés que	refrescarán	a sus paladares</textarea>
+    <textarea cols="68" rows="4" style="width:100%; padding:0 20px; text-align:justify;border:none;">A la llegada de los invitados se ofrecerá una barra de aguas frescas acompañadas de una selección especial de canapés que  refrescarán a sus paladares</textarea>
   </div>   
-    <div style="width:100%; padding:0 15px; text-align:center;"><img src="../img/Ribbon.png" style="width:50%;" /></div>
+    <div style="width:100%; padding:0 15px; text-align:center;"><img src="../img/ribbon-inv.png" style="width:50%;" /></div>
+    <table align="center" border="0" cellspacing="0" cellpadding="0" style="width:100%;font-size:10px;margin-top:5px; padding:5 30px; text-align:center">';
+    $total=0;
+    foreach($articulos as $id=>$d){
+    $html.='
+        <tr>
+            <td class = "cursiva" style="width:55%; text-align:center">'. $d["nombre"].'</td>
+        </tr>
+    ';
+    }
+    $html.='
+    </table>
     <table align="center" border="0" cellspacing="0" cellpadding="0" style="width:100%;font-size:10px;margin-top:5px; padding:5 30px; text-align:center">
         <tr>';
             $total=0;
@@ -238,7 +249,7 @@ font-family: "Pacifico", sans-serif;
                 <td>
                     <table>
                         <tr>
-                            <td><img src="../img/articulos/'. $d["image"].'" width="150" /></td>
+                            <td><img src="../img/articulos/'. $d["image"].'" width="170" height="130" /></td>
                         </tr>
                         <tr>
                             <td class = "cursiva" style="width:55%; text-align:center">'. $d["nombre"].'</td>
@@ -249,25 +260,37 @@ font-family: "Pacifico", sans-serif;
             else {
                 $total = 0;
                 $html.='</tr></table>
-                <table align="center" border="0" cellspacing="0" cellpadding="0" style="width:100%;font-size:10px;margin-top:5px; padding:5 30px; text-align:center"> <tr>';
+                <table align="center" border="0" cellspacing="0" cellpadding="0" style="width:100%;font-size:10px;margin-top:5px; padding:5 30px; text-align:center">
+                <tr>
+                <td>
+                    <table>
+                        <tr>
+                            <td><img src="../img/articulos/'. $d["image"].'" width="170" height="130" /></td>
+                        </tr>
+                        <tr>
+                            <td class = "cursiva" style="width:55%; text-align:center">'. $d["nombre"].'</td>
+                        </tr>
+                    </table>
+                </td>
+                ';
             }
             $total++;
             }
             $html.='</tr></table>'; 
             $html.='
             <div style="width:100%; padding:5 20px; text-align:justify;">
-            A	continuación	te	presento	los	canapés	seleccionados	para	la	Bienvenida:
+            A   continuación    te  presento    los canapés seleccionados   para    la  Bienvenida:
         </div>
         <br/>
-        <div style="width:100%; padding:0 20px; text-align:justify;"><strong>Menú de  3	Tiempos:</strong></div>
+        <div style="width:100%; padding:0 20px; text-align:justify;"><strong>Menú de  3 Tiempos:</strong></div>
         <table cellpadding="0" cellspacing="0" style=" font-size:12px;width:100%; margin-top:10px; padding:0 20px;">
         <tr>
             <td style="width:100%;">
                 <div style="width:100%; padding 20px; font-size:12px;text-align:justify;">
-                Al	llegar	a	sus	hermosas	mesas	decoradas	de	forma	muy	especial,	se	les	ofrecerá	a	los	invitados	un	
-menú	de	3	tiempos	con	ingredietnes	especialmente	seleccionados	para	la	ocasión	con	deliciosos	
-ingredientes.	A	continuación	te	presento	dos	opciones	para	que	ustedes	elijan	cuál	les	gustaría	que	
-degustaran	sus	invitados:</div>
+                Al  llegar  a   sus hermosas    mesas   decoradas   de  forma   muy especial,   se  les ofrecerá    a   los invitados   un  
+menú    de  3   tiempos con ingredietnes    especialmente   seleccionados   para    la  ocasión con deliciosos  
+ingredientes.   A   continuación    te  presento    dos opciones    para    que ustedes elijan  cuál    les gustaría    que 
+degustaran  sus invitados:</div>
             </td>
         </tr>
     </table>
@@ -279,13 +302,13 @@ degustaran	sus	invitados:</div>
             </tr>
             <tr>
             <td style="width:100%; text-align:justify;">
-                La	siguiente	cotización	desglosa	los	servicios	propuestos	para	tu	evento	contemplando	250	invitados:
+                La  siguiente   cotización  desglosa    los servicios   propuestos  para    tu  evento  contemplando    250 invitados:
             </td>
         </tr>        
     </table>
             <table align="center" border="0.3" cellspacing="0" cellpadding="0" style="width:100%;font-size:10px;margin-top:5px; padding:5 30px; text-align:center">
             <tr>
-                <td colspan="3" style="background-color:#78343B; text-align:center; color:#FFF"><strong>Cotización	del	Evento</strong></td>
+                <td colspan="3" style="background-color:#78343B; text-align:center; color:#FFF"><strong>Cotización  del Evento</strong></td>
                 </tr>
                 <tr align="center">
                     <th style="width:55%;">Desgloce de Servicio</th>
@@ -334,6 +357,7 @@ $filename="generador.pdf";
 //$orientar=$_POST["orientar"];
 $orientar="portrait";
 
+//echo $html;
 $topdf=new HTML2PDF($orientar,array($mmCartaW,$mmCartaH),'es');
 $topdf->writeHTML($html);
 $topdf->Output();
